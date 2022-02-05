0dd24b5b01e6dac4a0a0aedb159da076050d288c
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-02-05 16:28:37 -0500

    Update quotes and new-quote pages

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Omer",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Ozzy",
+    text: "Learning React is great!",
+  },
+];
+
 function AllQuotes() {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 }
 
 export default AllQuotes;

cd80ea6984b4745e6b8d13530081aee464a515c3
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-02-06 13:31:11 -0500

    Add sort quotes feature

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import QuoteForm from "../components/quotes/QuoteForm";
 
 function NewQuote() {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
